39a012cc082c9432e664778c692f70f5137eb73b
Juan Gomez <atilag@gmail.com>
2023-01-29 18:55:23 -0500

    Cleaning up the code and refactoring into a more functional style. The number of top changed files is now a parameter. Everything is working now, even the integration test.

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,15 @@
 use anyhow::Result;
-use async_trait;
 
-use octocrab::{models::repos::RepoCommit, Octocrab};
+use itertools::Itertools;
+use octocrab::{models::repos::{RepoCommit}, Octocrab};
 use std::collections::HashMap;
-use std::vec::Vec;
 use futures::{stream, StreamExt};
-
-fn sort_by_change_frequency(commits: Vec<RepoCommit>) -> HashMap<String, usize> {
-    let filename_and_counts: Vec<String> = commits
-        .into_iter()
-        .map(|repo_commit| repo_commit.files.unwrap_or_default())
-        //.map(|files| files)
-        .flat_map(|files| files)
-        .map(|file| file.filename)
-        .collect();
-
-    println!("Print");
-    filename_and_counts.iter().for_each(|e| println!("e: {e}"));
-    println!("End Print");
-    HashMap::new()
-}
 
 #[async_trait::async_trait]
 trait TopChangedFilesExt {
-    async fn get_top_5_changed_files(
+    async fn get_top_changed_files(
         &self,
+        num_of_files: usize,
         owner: &str,
         repo: &str,
     ) -> Result<HashMap<String, usize>>;
@@ -32,42 +17,31 @@ trait TopChangedFilesExt {
 
 #[async_trait::async_trait]
 impl TopChangedFilesExt for Octocrab {
-    async fn get_top_5_changed_files(
+    async fn get_top_changed_files(
         &self,
+        number_of_files: usize,
         owner: &str,
         repo: &str,
     ) -> Result<HashMap<String, usize>> {
         let commits = self.repos(owner, repo).list_commits().send().await?;
-        let commits_stream = stream::iter(commits)
-        .collect::<Vec<RepoCommit>>().await;
-
-        let shas = stream::iter(commits_stream)
-        // .map(|repo_commit| async {
-        //         let c: octocrab::models::repos::Commit =
-        //             self.get(repo_commit.url, None::<&()>).await.unwrap();
-        //         //println!("c: {}", c.sha.unwrap());
-        //        if let sha = Some(c.sha) {
-        //         sha
-        //        }
-        // });
-        .map(|repo_commit| async {
-            println!("Calling {}", repo_commit.url);
-            let commit: octocrab::models::repos::Commit = self.get(repo_commit.url, None::<&()>).await?;
-            println!("Got some results!");
-            Ok(commit) as Result<octocrab::models::repos::Commit, anyhow::Error>
+        let commits_stream = stream::iter(commits);
+        let changed_files: HashMap<String, usize> = commits_stream
+        .filter_map(|repo_commit| async move { 
+            self.get(repo_commit.url, None::<&()>).await.ok() as Option<RepoCommit>
         })
-        .for_each(|commit| async {
-            match commit.await {
-                 Ok(commit) => match commit.sha {
-                    Some(sha) => println!("sha: {}", sha),
-                    None => println!("no sha!"),
-                 },
-                 Err(error) => println!("Error: {}", error),
-            }
+        .flat_map(|commit| stream::iter(commit.files))
+        .flat_map(|diff_entries| stream::iter(diff_entries))
+        .fold(HashMap::new(), |mut changed_files, diff_entry| async move {
+            *changed_files.entry(diff_entry.filename).or_insert(0) += 1;
+            changed_files
         })
-        .await;
+        .await
+        .into_iter()
+        .sorted_by(|a, b| b.1.cmp(&a.1))
+        .take(number_of_files)
+        .collect::<HashMap<String, usize>>();
 
-        Ok(HashMap::new())
+        Ok(changed_files)
     }
 }
 
@@ -85,21 +59,20 @@ mod test {
         let octocrab = setup().unwrap();
 
         let top_5_changed_files = octocrab
-            .get_top_5_changed_files("atilag", "IBM-Quantum-Systems-Exercise")
+            .get_top_changed_files(5, "atilag", "IBM-Quantum-Systems-Exercise")
             .await;
 
         let expected: HashMap<String, usize> = [
             ("LICENSE".into(), 1),
-            ("README.md".into(), 1),
-            ("generate-quantum-programs.py".into(), 1),
-            ("large_quantum_program_input.json".into(), 1),
-            ("quantum_program_input.json".into(), 1),
+            ("README.md".into(), 15),
+            ("generate-quantum-programs.py".into(), 7),
+            ("large_quantum_program_input.json".into(), 4),
+            ("quantum_program_input.json".into(), 3),
         ]
         .iter()
         .cloned()
         .collect();
 
-        //assert_eq!(expected, top_5_changed_files.unwrap());
-        assert!(false);
+        assert_eq!(expected, top_5_changed_files.unwrap());
     }
 }
