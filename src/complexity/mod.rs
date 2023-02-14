5ec6678f2dfd82e0bc2949b0fffcfb3eec477287
Juan Gomez <atilag@gmail.com>
2023-02-13 21:39:38 -0500

    Testing the execution of cargo clippy. It's not working so far :(

--- src/complexity/mod.rs
+++ src/complexity/mod.rs
@@ -11,9 +11,8 @@ pub fn compute_cognitive_index(prog_lang: ProgrammingLang, file: PathBuf) -> Res
 pub enum ProgrammingLang {
     Rust,
     Python,
-    Go
+    Go,
 }
-
 
 trait LangEvaluator {
     fn eval(&self, file: PathBuf) -> Result<u16>;
@@ -27,36 +26,55 @@ impl LangEvaluator for RustLangEvaluator {
             .arg("--")
             .arg("-A clippy::all")
             .arg("-D clippy::cognitive_complexity")
-            .arg(format!("--file {file_path}", ))
-            .output()
-            .map_err(|error|{
-                println!("Error: {error}");
-            });
-       Ok(9)
+            .arg("--file ")
+            .arg(file_path)
+            .output();
+        //.map_err(|error| {
+        //    println!("Error: {error}");
+        //});
+
+        match output {
+            Ok(num) => println!("Ok({:#?})", num),
+            Err(error) => println!("Error: {:#?}", error),
+        };
+
+        Ok(9)
     }
-    
 }
 
+struct PythonLangEvaluator;
+impl LangEvaluator for PythonLangEvaluator {
+    fn eval(&self, file: PathBuf) -> Result<u16> {
+        let file_path = file.into_os_string().into_string().unwrap();
+        let output = Command::new("flake8")
+            .arg("--select CCR001")
+            .arg("--max-cognitive-complexity=1")
+            .arg(format!("{file_path}"))
+            .output()
+            .map_err(|error| {
+                println!("Error: {error}");
+            });
+        Ok(9)
+    }
+}
 
 // Factory function to create language evaluators.
 fn create_lang_evaluator(prog_lang: ProgrammingLang) -> Box<dyn LangEvaluator> {
     match prog_lang {
-        ProgrammingLang::Rust => Box::new(RustLangEvaluator{}),
-        _ => panic!("Language evaluator not implemented yet!")
+        ProgrammingLang::Rust => Box::new(RustLangEvaluator {}),
+        _ => panic!("Language evaluator not implemented yet!"),
     }
 }
 
-
-
 #[cfg(test)]
 mod test {
+    use std::io::Write;
+
     use super::*;
+    use tempfile::NamedTempFile;
 
     #[tokio::test]
     async fn calculate_cognitive_complexity_of_a_rust_file() {
-
-        
-
         let complex_block_of_code = "
             let mut b  = 5;
             for i in 1..=10 {
@@ -81,10 +99,14 @@ mod test {
             }
         ";
 
-
+        let mut temp_rust_file = NamedTempFile::new().unwrap();
+        temp_rust_file
+            .write_all(complex_block_of_code.as_bytes())
+            .unwrap();
 
         let expected = 9;
-        let cognitive_complex_index = compute_cognitive_index(ProgrammingLang::Rust, PathBuf::from("Hola")).unwrap();
+        let cognitive_complex_index =
+            compute_cognitive_index(ProgrammingLang::Rust, temp_rust_file.path().into()).unwrap();
 
         assert_eq!(expected, cognitive_complex_index);
     }
